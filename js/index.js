10cebf9191c7a42d769dd6bb6afceeaf0ddf6407
cahuizar <hire@carloshuizar.com>
2018-05-05 18:57:30 -0400

    Contact section created

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
     // animate
     $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - 200
       }, 1000, function(){
 
         // when done, add hash to url
